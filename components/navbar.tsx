fd7ca10f77ad6c1799b1a57641458132f370cb39
PremTak01 <premtak2002@gmail.com>
2024-05-01 18:08:47 +0530

    theming

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,14 +16,15 @@ import { ThemeToggle } from "./theme-toggle";
 export default function Navbar({}): any {
   return (
     <nav className="fixed z-50 flex items-center justify-between w-full h-24 px-4 py-10 backdrop-blur-md bg-background bg-opacity-30 md:px-8 lg:px-12 xl:px-16 2xl:px-24">
-      <Link href="/">
+      <Link href="/" className="min-h-screen flex justify-center items-center">
         <Image
           alt="logo"
-          className="w-40 cursor-pointer dark:invert"
-          src="/images/logo.svg"
+          className="w-20 cursor-pointer"
+          src="/images/logo.png"
           height={100}
           width={170}
         />
+        <p className="mt-3 text-3xl font-thin hover:font-extrabold">File Flex</p>
       </Link>
       <div className="hidden gap-1 md:gap-2 lg:gap-4 md:flex">
         <Button variant="ghost" className="font-semibold text-md">
@@ -45,7 +46,7 @@ export default function Navbar({}): any {
         <Link target="_blank" href="https://github.com/PremTak01/file-flex">
           <Button
             variant="default"
-            className="items-center hidden gap-2 bg-orange-600 rounded-full w-fit md:flex"
+            className="items-center hidden gap-2 rounded-full w-fit md:flex"
             size="lg"
           >
             <span>Github Repo</span>
